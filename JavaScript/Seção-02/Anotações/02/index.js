ae8e015740b8fcda97603205091d0b2a1d683c77
unknown <ivnymetzker@gmail.com>
2023-01-12 18:28:28 -0300

    Aula 02

---
+++ JavaScript/Seção-02/Anotações/02/index.js
@@ -0,0 +1,18 @@
+// Introduzindo o conceito de variáveis
+
+let nome = 'João';
+
+console.log(nome, 'nasceu em 1984.');
+console.log('Em 2000', nome, 'conheceu Maria.');
+console.log(nome, 'casou-se com Maria em 2012.');
+console.log('O filho de', nome, 'chama-se Eduardo.');
+
+// Também é possível apenas declarar uma variável sem valor
+
+let nome1; // Declarando
+console.log(nome1);
+
+nome1 = 'Ivny'; // Atribuindo valor
+console.log(nome1);
+nome1 = 'Metzker'; // Reatribuindo valor
+console.log(nome1);
--- JavaScript/Seção-02/Exercicios/01/index.js
+++ JavaScript/Seção-02/Exercicios/01/index.js
@@ -1,2 +1,3 @@
 
 console.log (`Meu nome é "NOME". Estou aprendendo Javascript às ${10} da manhã.`);
+console.log("Hello World");
