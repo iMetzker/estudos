cd275df1eda146e8a10eb7a4cf8e5c9a30283751
unknown <ivnymetzker@gmail.com>
2023-01-13 22:59:28 -0300

    aula 04 exercicio

--- JavaScript/Seção-02/Anotações/04/js/index.js
+++ JavaScript/Seção-02/Anotações/04/js/index.js
@@ -1,1 +1,12 @@
-alert('Olá Mundo!');
+// Utilizando o console do navegador
+
+alert('Bem vindo! Vamos fazer uma conta simples, digite alguns números e vamor ver o resultado: numero 01 + numero 02 * numero 3')
+
+const num01 = prompt('Digite um número: ');
+const num02 = prompt('Digite outro número: ');
+const num03 = prompt('Digite seu último número: ');
+
+const resultado = (Number(num01) + Number(num02)) * Number(num03);
+
+alert(resultado);
+
