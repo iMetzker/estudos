755726ec6323578dabb3d1da2eba651cf4236987
Ivny Metzker <ivnymetzker@gmail.com>
2023-01-17 20:09:09 -0300

    aula 27 - anotação 07, OBJETO MATH

--- JavaScript/Seção-02/Anotações/07/script.js
+++ JavaScript/Seção-02/Anotações/07/script.js
@@ -1,3 +1,23 @@
 // AULA 27 - Objeto Math
 
-// verificação //
+let num1 = 9.54578;
+
+//Arredondar o valor para baixo
+let num2 = Math.floor(num1); 
+console.log("Numero: " + num2);
+
+//Arredondar o valor para cima
+num2 = Math.ceil(num1); 
+console.log("Numero: " + num2);
+
+//Arredonda o valor para o numbero mais próximo, .5 ^
+num2 = Math.round (num1); 
+console.log("Número: " + num2);
+
+//Mostra qual maior número
+console.log("Maior número: " + Math.max(1,2,3,4,5, -10, -50, 1500, 9)); 
+
+//Gerando um número aleatório entre 5 e 10
+const aleatorio = Math.round(Math.random() * (10 - 5) + 5);
+console.log(`Número aleatório: ${aleatorio}`);
+
