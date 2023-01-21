608a5827b4931819b06616a3857b14318686a234
Ivny Metzker <ivnymetzker@gmail.com>
2023-01-21 18:26:59 -0300

    Aula 39 - Short-Circuit Detalhes

--- JavaScript/Seção-03/Anotações/01/script.js
+++ JavaScript/Seção-03/Anotações/01/script.js
@@ -1,7 +1,20 @@
-// AVALIAÇÃO DE CURTO CIRCUITO
+// AULA 39 - AVALIAÇÃO DE CURTO CIRCUITO
 
+
+// CHECANDO OS VALORES E O PRIMEIRO VERDADEIRO ELE RETORNA, MUDANDO DE NULL PARA ALGUM VALOR, JÁ O RETORNA. SE CASO TODOS FOREM VALORES FALSE, O VALOR QUE SERÁ RETORNADO É O ÚLTIMO.
 const corUsuario = null;
 const corPadrao = corUsuario || 'preto'; 
-// ^ CHECA OS VALORES E O PRIMEIRO VERDADEIRO ELE RETORNA, MUDANDO DE NULL PARA ALGUM VALOR, JÁ O RETORNA
 
 console.log(corPadrao);
+
+
+// VALORES QUE RETORNAM FALSE = STRING VAZIA, 0, NULL E NAN.
+//OPERADOR || - OR. 
+
+const a = 0;
+const b = null;
+const c = 'false';
+const d = false;
+const e = NaN;
+
+console.log(a || b || c || d || e);
