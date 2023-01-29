9f0b9f1b2260d9f70387c3420a9478277470a5aa
Ivny Metzker <ivnymetzker@gmail.com>
2023-01-29 20:46:27 -0300

    Aula 56 - Introdução ao forEach

--- JavaScript/Seção-03/Anotações/10/02-script.js
+++ JavaScript/Seção-03/Anotações/10/02-script.js
@@ -1,2 +1,8 @@
-// FOREACH
+// FOREACH - INTRODUÇÃO
 
+const nomes = ['Ivny', 'Luan', 'Catarina'];
+
+// PODEMOS UTILIZAR O forEach SEMPRE QUE TIVERMOS UM ARRAY
+nomes.forEach(function(valor, indice, array){
+    console.log(valor, indice, array);
+});
