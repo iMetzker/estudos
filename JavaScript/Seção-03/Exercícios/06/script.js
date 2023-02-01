c33a8ca7303acd39ca7b7cef4ae633db1a639de6
Ivny Metzker <ivnymetzker@gmail.com>
2023-01-31 21:32:28 -0300

    Aula 61 - Exercício Treino de Lógica pt2

---
+++ JavaScript/Seção-03/Exercícios/06/script.js
@@ -0,0 +1,4 @@
+// AULA 61 - TREINO DE LÓGICA (UMA FUNÇÃO QUE RECEBE UMA IMAGEM QUE SE ESTIVER NO MODO PAISAGEM RETORNAR TRUE)
+
+const ePaisagem = (largura, altura)  => largura >=  altura; // ? true : false
+console.log(ePaisagem(1080,1920));
