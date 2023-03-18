2c73866abd422ed4af9edce0dff141c69652a78c
Ivny Metzker <ivnymetzker@gmail.com>
2023-03-17 22:39:57 -0300

    Aula 73 - Callback

---
+++ udemy/JavaScript/Seção-04/Anotações/06.js
@@ -0,0 +1,49 @@
+// AULA 73 - CALLBACK
+
+function rand(min = 1000, max = 3000) {
+  const num = Math.random() * (max - min) + min;
+  return Math.floor(num);
+}
+
+function f1(callback) {
+  setTimeout(function () {
+    console.log("f1");
+    if (callback) callback();
+  }, rand());
+}
+
+function f2(callback) {
+  setTimeout(function () {
+    console.log("f2");
+    if (callback) callback();
+  }, rand());
+}
+
+function f3(callback) {
+  setTimeout(function () {
+    console.log("f3");
+    if (callback) callback();
+  }, rand());
+}
+
+// ADICIONANDO O IF CALLBACK ( QUE É  UMA FUNÇÃO), PRIMEIRO SERÁ VERIFICADO SE TEM A FUNÇÃO E LOGO APÓS SERÁ EXECUTADO, DEFIMOS NESSA FUNÇÃO A FUNÇÃO QUE SERÁ CHAMADA EM SEGUIDA
+f1(function () {
+  f2(function () {
+    f3(function () {
+      console.log("Olá Mundo!");
+    });
+  });
+});
+
+//PODERIA SER ESCRITO DE UMA FORMA MAIS LIMPA:
+f1(f1Callback);
+function f1Callback() {
+  f2(f2Callback);
+}
+
+function f2Callback() {
+  f3(f3Callback);
+}
+function f3Callback() {
+  console.log("Cheguei aqui");
+}
