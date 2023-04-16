cd8520d9fddb3b86127ec0daa4b8bc83c4424158
Ivny Metzker <ivnymetzker@gmail.com>
2023-04-16 20:14:38 -0300

    Aula 91 - Setter

--- udemy/JavaScript/Seção-06/Anotações/03.js
+++ udemy/JavaScript/Seção-06/Anotações/03.js
@@ -1,22 +1,35 @@
 // AULA 91 - GETTERS E SETTERS ( SEGUINDO COM OBJECT.DEFINEPROPERTY)
 
-// GETTER É PARA OBTER UM VALOR
-// SETTER É PARA SETAR UM VALOR (CONFIGURÁ-LO)
+// GETTER É PARA OBTER UM VALOR (UM MÉTODO DENTRO DA FUNÇÃO CONSTRUTORA)
+// SETTER É PARA SETAR UM VALOR (CONFIGURÁ-LO) // VALIDAR O VALOR
 
 function Produto(nome, preco, estoque) {
     this.nome = nome;
     this.preco = preco;
-    this.estoque = estoque;
+    
+    let estoquePrivado = estoque;
   
     Object.defineProperty(this, "estoque", {
       enumerable: true,
-      value: estoque,
-      writable: true,
       configurable: false,
+      get: function () {
+        return estoquePrivado;
+      },
+      set: function (valor) {
+
+        // VALIDAR SE O VALOR É UM NUMERO
+        if (typeof valor !== 'number') {
+          //console.log('Bad value'); AO INVÉS DE EXIBIR NA TELA, VAMOS CRIAR UM TYPE ERROR
+          throw new TypeError ('Bad value')
+        }
+
+        estoquePrivado = valor;
+      }
     });
   }
   
   const p1 = new Produto("Camiseta", 20, 3);
-  console.log(p1);
+  p1.estoque = 'afsg';
+  console.log(p1.estoque);
   
 
