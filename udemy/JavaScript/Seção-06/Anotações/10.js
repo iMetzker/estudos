c5d328a9915e0b0a27647fdd8b8a0ca2b18adc3f
Ivny Metzker <ivnymetzker@gmail.com>
2023-08-12 00:04:23 -0300

    Aula 101 - Estrutura de Dados new Map()

---
+++ udemy/JavaScript/Seção-06/Anotações/10.js
@@ -0,0 +1,26 @@
+// 101 - OBJETO MAP()
+
+const pessoas = [
+  { id: 3, nome: "Luiz" },
+  { id: 2, nome: "Maria" },
+  { id: 1, nome: "Helena" },
+];
+
+// const novasPessoas = {};
+//  for (const pessoa of pessoas) {
+//  const { id } = pessoa;
+//   novasPessoas[id] = { ...pessoa};
+//    };
+
+const novasPessoas = new Map();
+for (const pessoa of pessoas) {
+  const { id } = pessoa;
+  novasPessoas.set(id, { ...pessoa});
+}
+
+console.log(novasPessoas);
+
+// ITERANDO E OBTENDO UM ARRAY
+for (const [identifier, { id, nome}] of novasPessoas) {
+    console.log(identifier, id, nome);
+}
