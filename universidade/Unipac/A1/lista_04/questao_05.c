a5c1ca43261dc9f532e06327a442be4ce5edd7a3
Metzker <ivnymetzker@gmail.com>
2024-10-11 16:55:32 -0300

    feat: list 05
    adding question 5

--- universidade/Unipac/A1/lista_04/questao_05.c
+++ universidade/Unipac/A1/lista_04/questao_05.c
@@ -2,3 +2,50 @@
 Logo após deve ser solicitado a digitação de um valor e esse valor deverá ser pesquisado
 nessa lista utilizando BUSCA BINÁRIA. */
 
+#include <stdio.h>
+
+int main() {
+    int n, i, valor, inicio, fim, meio, encontrado = 0;
+    // int arr[n];
+
+    printf("Digite a quantidade de elementos que a sua lista terá: ");
+    scanf("%d", &n);
+    
+    int arr[n];
+
+    printf("Digite os elementos da sua lista em ordem crescente: \n");
+    for (i = 0; i < n; i++) {
+        printf("%d item - ", i);
+        scanf("%d", &arr[i]);
+    }
+    
+    printf("Digite o valor a ser pesquisado: ");
+    scanf("%d", &valor);
+
+
+    inicio = 0;
+    fim = n - 1;
+
+    while (inicio <= fim) {
+        meio = (inicio + fim) / 2;
+
+        if (arr[meio] == valor) {
+            printf("Valor %d encontrado na posição %d da lista.\n", valor, meio);
+            encontrado = 1;
+            break;
+        } else if (arr[meio] < valor) {
+            inicio = meio + 1;
+        } else {
+            fim = meio - 1;
+        }
+    }
+
+    if (!encontrado) {
+        printf("Valor %d não encontrado na lista.\n", valor);
+    }
+
+    return 0;
+}
+
+
+
